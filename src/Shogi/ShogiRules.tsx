f79f7bc6ff5e608627dadf59c12c5d81ce5b0eb1
David Gabor Bodor <david.gabor.bodor@gmail.com>
2019-05-16 18:23:11 +0200

    shogi: add piece outline

--- src/Shogi/ShogiRules.tsx
+++ src/Shogi/ShogiRules.tsx
@@ -1,4 +1,4 @@
-import {BoardGameRules, BoardState} from '../BoardGame/BoardGame';
+import {BoardGameRules, BoardState, PieceRules} from '../BoardGame/BoardGame';
 
 import './Shogi.css';
 
@@ -8,15 +8,38 @@ export {rules};
 const BLACK = "react-boardgame__piece--color-black";
 //const NOCOLOR = "react-boardgame__piece--color-nocolor";
 
+const KING = '王';
+
 
 interface ShogiBoardState extends BoardState {
 
 }
 
+class KingRules {
+  static figure = "王";
+  static validMoves(_board: ShogiBoardState, _square: string): Array<string> {
+    return [];
+  }
+  static movePiece(board: ShogiBoardState, _square: string, _newSquare: string): ShogiBoardState {
+    return board;
+  }
+}
+
+function initialBoardState(): ShogiBoardState {
+  return {
+    pieces: {
+      "e1": {pieceType: KING, color: BLACK},
+    },
+    activeSide: BLACK,
+  }
+}
+
 const rules: BoardGameRules<ShogiBoardState> = {
   board: {ranks: 9, files: 9, style: 'shogi'},
-  pieces: {},
-  initialBoardState: () => ({pieces: {}, activeSide: BLACK}),
+  pieces: {
+    [KING]: KingRules
+  },
+  initialBoardState: initialBoardState,
   selectors: [],
   sideIndicators: [],
   emptySquareMove: (board: ShogiBoardState, _: string) => board,
