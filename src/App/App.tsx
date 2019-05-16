4c1f4137a7a63cbaf6e161171adbf7ca2aa9cc6f
David Gabor Bodor <david.gabor.bodor@gmail.com>
2019-05-16 17:05:05 +0200

    App: add Shogi game

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -6,11 +6,17 @@ import {BoardGame} from '../BoardGame/BoardGame';
 import {rules as chessRules} from '../Chess/ChessRules';
 import {rules as checkersRules} from '../Checkers/CheckersRules';
 import {rules as goRules} from '../Go/GoRules';
+import {rules as shogiRules} from '../Shogi/ShogiRules';
 
 class App extends Component {
   render() {
     return (
       <div className="App react-boardgame__main-container">
+      <div className="react-boardgame__inline-container">
+        <ErrorBoundary errorMessage="An error occured, we misplayed the shogi board. Care to play a different game?">
+          <BoardGame rules={shogiRules} />
+        </ErrorBoundary>
+      </div>
         <div className="react-boardgame__inline-container">
           <ErrorBoundary errorMessage="An error occured, we misplayed the go board. Care to play a different game?">
             <BoardGame rules={goRules} />
