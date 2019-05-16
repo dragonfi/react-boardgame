0f232a850242be7f3632b2329e8c80b6f0a34217
David Gabor Bodor <david.gabor.bodor@gmail.com>
2019-05-16 18:29:44 +0200

    shogi: add gold general figure

--- src/Shogi/ShogiRules.tsx
+++ src/Shogi/ShogiRules.tsx
@@ -4,11 +4,12 @@ import './Shogi.css';
 
 export {rules};
 
-//const WHITE = "react-boardgame__piece--color-white";
+const WHITE = "react-boardgame__piece--color-white";
 const BLACK = "react-boardgame__piece--color-black";
 //const NOCOLOR = "react-boardgame__piece--color-nocolor";
 
-const KING = '王';
+const KING = "王";
+const GOLD = "金";
 
 
 interface ShogiBoardState extends BoardState {
@@ -25,10 +26,19 @@ class KingRules {
   }
 }
 
+class GoldRules extends KingRules {
+  static figure = "金";
+}
+
 function initialBoardState(): ShogiBoardState {
   return {
     pieces: {
       "e1": {pieceType: KING, color: BLACK},
+      "d1": {pieceType: GOLD, color: BLACK},
+      "f1": {pieceType: GOLD, color: BLACK},
+      "e9": {pieceType: KING, color: WHITE},
+      "d9": {pieceType: GOLD, color: WHITE},
+      "f9": {pieceType: GOLD, color: WHITE},
     },
     activeSide: BLACK,
   }
@@ -37,7 +47,8 @@ function initialBoardState(): ShogiBoardState {
 const rules: BoardGameRules<ShogiBoardState> = {
   board: {ranks: 9, files: 9, style: 'shogi'},
   pieces: {
-    [KING]: KingRules
+    [KING]: KingRules,
+    [GOLD]: GoldRules,
   },
   initialBoardState: initialBoardState,
   selectors: [],
